e0e15b3a63f84734eca97ee84bcda2ea912cbd21
az1z1ally <albyally5@gmail.com>
2024-04-08 10:57:07 +0300

    Moving fetch task code to fetch.js

--- MonsterLesson/fetch.js
+++ MonsterLesson/fetch.js
@@ -24,3 +24,35 @@
 // }
 
 // window.createPost = createPost // Assing createPost() to the window property so that we can use it inside console(make it global), b'se we are using (it levaerage module) workflow, it's not necessary if you are using a normal JS workflow
+
+
+// Add post from text inputs using API after clicking a button
+const postInput = document.querySelector(".postInput")
+const postViews = document.querySelector(".postViews")
+const postBtn = document.querySelector(".postBtn")
+
+// Binding click event to the button
+postBtn.addEventListener("click", async () => {
+	if (postInput.value === "" || postViews.value === "") {
+		alert("Add some values in the input box before proceeding ")
+		return
+	}
+
+	// Adding a post
+	try {
+		await fetch("http://localhost:3000/posts", {
+			method: "POST",
+			body: JSON.stringify({title: postInput.value, views: Number(postViews.value)}),
+			headers: {
+		 		"Content-Type": "application/json"
+		 	}
+		})
+	} catch(err){
+		console.error("Something is wrong!")
+	}
+
+	// Clear input boxes after submitting
+	postInput.value = ""
+	postViews.value = ""	
+ 
+})
