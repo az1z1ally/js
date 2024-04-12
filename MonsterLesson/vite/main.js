66122ad66cb1b652ee752440426c311859985740
az1z1ally <albyally5@gmail.com>
2024-04-12 12:20:44 +0300

    Completed Weather app project

--- MonsterLesson/vite/main.js
+++ MonsterLesson/vite/main.js
@@ -1,5 +1,12 @@
-import { getUserNames, bar, foo } from "./utils"
-import secretString, { baz } from "./constants"
+// import { getUserNames, bar, foo } from "./utils"
+// import secretString, { baz } from "./constants"
 
-const usernames = getUserNames([{id: "1", name: "JS"}])
-console.log(usernames,bar, foo, secretString, baz)
+// const usernames = getUserNames([{id: "1", name: "JS"}])
+// console.log(usernames, bar, foo, secretString, baz)
+
+import "./weatherapp"
+
+
+
+
+
